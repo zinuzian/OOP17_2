e9fa10f80aa3b5a57b115363cd40390c4b5d94fa
zinuzian <zinuzian@naver.com>
2017-10-23 21:55:33 +0900

    Add files via upload

--- prob1/prob1/inf_int.cpp
+++ prob1/prob1/inf_int.cpp
@@ -18,8 +18,9 @@
 
 inf_int::inf_int()              // assign 0 as a default value
 {
-	digits = new char[1];
-	digits[0] = 0;
+	digits = new char[2];
+	digits[0] = '0';
+	digits[1] = 0;
 	length = 1;
 	thesign = true;
 }
@@ -28,69 +29,67 @@ inf_int::inf_int(int n)
 	thesign = (n < 0) ? false : true;
 	if (!thesign) n = -n;
 	string s = to_string(n);
+	reverse(s.begin(), s.end());
 	length = s.length();
-	digits = new char[length];
-	for (int i = 0; n; i++){
-		digits[i] = n % 10 + '0';
-		n /= 10;
-	}
+	digits = new char[length + 1];
+	s.copy(digits, length, 0);
+	digits[length] = 0;
 }
 inf_int::inf_int(const char* str)
 {
 	if (str[0] == '-') thesign = false;
-	string s = (thesign) ? str : str + 1;
+	else thesign = true;
+	string s = string((thesign) ? str : str + 1);
+	reverse(s.begin(), s.end());
 	length = s.length();
-	digits = new char[length];
-	for (int i = 0; i < length; i++){
-		digits[i] = s.at(length - i - 1);
-	}
+	digits = new char[length+1];
+	s.copy(digits, length, 0);
+	digits[length] = 0;
 }
 inf_int::inf_int(const inf_int& that) // copy constructor
 {
 	length = that.length;
 	if (length > 0){
 		thesign = that.thesign;
-		digits = new char[length];
+		digits = new char[length+1];
 		for (int i = 0; i < length; i++){
 			digits[i] = that.digits[i];
 		}
+		digits[length] = 0;
 	}
-	else{
+	else{		//error
+		digits = new char[2];
+		digits[0] = '0';
+		digits[1] = 0;
+		length = 1;
 		thesign = true;
-		length = 0;
 	}
 }
 inf_int::~inf_int() // destructor
 {
-	delete[] digits;
+	delete[] this->digits;
 }
 
 inf_int& inf_int::operator=(const inf_int& that) // assignment operator
 {
-	this->thesign = that.thesign;
 	delete[] this->digits;
+	this->thesign = that.thesign;
+	this->digits = new char[that.length+1];
 	this->length = that.length;
-	this->digits = new char[this->length];
-	for (int i = 0; i < that.length; i++){
-		this->digits[i] = that.digits[i];
-	}
+	(string(that.digits)).copy(this->digits, this->length, 0);
+	this->digits[this->length] = 0;
 	return *this;
 }
 
 bool operator==(const inf_int& a, const inf_int& b)
 {
-	int i;
 	if (a.thesign == b.thesign){				//same sign
-		if (a.length == b.length){				//same length
-			for (i = 0; i < a.length; i++){		//same digits
-				if (a.digits[i] != b.digits[i])
-					break;
-			}
-		}
+		string s = string(a.digits);
+		if (s.compare(b.digits) == 0)			//same digits
+			return true;
 	}
-	if (i == a.length)
-		return true;
 	return false;
+
 }
 bool operator!=(const inf_int& a, const inf_int& b)
 {
@@ -124,39 +123,10 @@ bool operator>(const inf_int& a, const inf_int& b)
 			}
 		}
 	}
-	return false;			//1) ->+   2) long+>short+	3)short->long-	4)equal
+	return false;			// 1) ->+	2) short+>long+	3)long->short-	4)equal
 		
 }
 bool operator<(const inf_int& a, const inf_int& b){
-	/*
-	if (a.thesign ^ b.thesign){				//sign is diffrent
-		if (b.thesign){						//-<+
-			return true;
-		}
-		else{								//+<-
-			//return false;
-		}
-	}
-	else{									//sign is same
-		if (a.thesign){						//+<+
-			if (a.length < b.length) return true;
-			//if (a.length > b.length) return false;
-			for (int i = a.length - 1; i >= 0; i--){	//same length
-				if (a.digits[i] > b.digits[i]) return false;
-				if (a.digits[i] < b.digits[i]) return true;
-			}
-		}
-		else{								// -<-
-			if (a.length > b.length) return true;
-			//if (a.length < b.length) return false;
-			for (int i = a.length - 1; i >= 0; i--){
-				if (a.digits[i] < b.digits[i]) return false;
-				if (a.digits[i] > b.digits[i]) return true;
-			}
-		}
-	}
-	return false;			//1) ->+   2) long+>short+	3)short->long-	4)equal
-	*/
 	if (a == b)
 		return false;
 	return !(a > b);
@@ -164,66 +134,130 @@ bool operator<(const inf_int& a, const inf_int& b){
 
 inf_int operator+(const inf_int&a , const inf_int& b)
 {	
-	inf_int r;
-	string tmp_r;
-	inf_int abs_a(a), abs_b(b);
-	abs_a.thesign = true; abs_b.thesign = true;
-	const inf_int *pa, *pb;					//let pa's abs is bigger
-	if (abs_a>abs_b){
-		pa = &a; pb = &b;
-	}
-	else{
-		pa = &b; pb = &a;
-	}
+	if (a.thesign == b.thesign){		//same sign
+		const inf_int *hi = a.length > b.length ? &a:&b;
+		const inf_int *lo = a.length > b.length ? &b : &a;
+		string str = string(hi->digits);
 
-	if (a.thesign ^ b.thesign){			//diffrent sign(=subtract)
-
-		for (int i = 0; i < pb->length; i++){
-			pa->digits[i] -= pb->digits[i];
-		}
-		for (int i = 0; i < pa->length; i++){
-			if (pa->digits)
-		}
-		if (pa->digits[pa->length-1]<0)
-			
-	}
-	else{									//same sign
-		if (abs_a.length == abs_b.length
-			&& abs_a.digits[abs_a.length - 1] + abs_b.digits[abs_b.length - 1] > 9)
-			r.length = pa->length + 1;
-		else
-			r.length = pa->length;
-		delete[] r.digits;
-		r.digits = new char[r.length];		//create new digits array
-		r.thesign = a.thesign;
-		for (int i = 0; i < r.length; i++){
-			r.digits[i] = 0;
+		for (int i = 0; i < lo->length; i++){		//add every single numbers
+			str.at(i) += (lo->digits[i]-'0');
 		}
 
-		for (int i = 0; i < pb->length; i++){
-			char addResult = abs_a.digits[i] + abs_b.digits[i];
-			if (addResult > 9){
-				r.digits[i+1]++;
+		bool carryExists = false;				//carry occurs -> add 1
+		for (int i = 0; i < str.length(); i++){
+			str.at(i) += carryExists ? 1 : 0;
+			if (str.at(i)>'9'){		//carry
+				str.at(i) -= 10;
+				carryExists = true;
 			}
-			r.digits[i] = addResult % 10;
+			else
+				carryExists = false;
+		}
+		if (carryExists)		//MSB carry?
+			str += '1';
+
+		reverse(str.begin(), str.end());
+		inf_int result(str.c_str());
+		result.thesign = hi->thesign;
+		return result;
+	}
+	else{								//different sign(=subtract)
+		if (a.thesign){			//b is minus
+			inf_int t(b);
+			t.thesign = true;
+			return a - t;
+		}
+		else{					//a is minus
+			inf_int t(a);
+			t.thesign = true;
+			return b - t;
 		}
 	}
-	return r;
+
 }
 inf_int operator-(const inf_int& a, const inf_int& b)
 {
-	inf_int tb(b);
-	tb.thesign = !b.thesign;
-	return a + tb;
+	if (a.thesign == b.thesign){			//same sign
+		const inf_int *hi=&a, *lo=&b;					//let hi's abs is bigger
+		bool isMinus = false;
+		if (a.length < b.length){						//longer one would be hi
+			hi = &b; lo = &a;
+			isMinus = true;
+		}
+		else if (a.length == b.length){					//same length
+			int i;
+			for (i = a.length - 1; i >= 0; i--){
+				if (a.digits[i] > b.digits[i]){ break; }
+				if (a.digits[i] < b.digits[i]){ hi = &b; lo = &a; isMinus = true; break; }
+			}
+			if (i < 0){
+				return inf_int();
+			}
+		}
+
+		string str = string(hi->digits);
+		for (int i = 0; i < lo->length; i++){
+			str.at(i) -= (lo->digits[i]-'0');
+		}
+		bool carryExists = false;
+		for (int i = 0; i < str.length(); i++){
+			str.at(i) -= carryExists ? 1 : 0;
+			if (str.at(i) < '0')				//carry
+			{
+				str.at(i) += 10;
+				carryExists = true;
+			}
+			else
+				carryExists = false;
+		}
+		if (carryExists)		//error
+			return *lo - *hi;	//change hi, lo
+
+		for (int i = str.length() - 1; i >= 0; i--){	//shrink to fit
+			if (str.at(i) != '0') break;
+			str = str.substr(0, str.length() - 1);
+		}
+		reverse(str.begin(), str.end());
+		inf_int result(str.c_str());
+		result.thesign = !isMinus;
+		return result;
+	}
+	else{									//different sign(=add)
+		if (a.thesign){				//b is minus
+			inf_int t(b);
+			t.thesign = true;
+			return a + t;
+		}
+		else{
+			inf_int t(a);			//a is minus
+			t.thesign = true;
+			return inf_int() - (t + b);
+		}
+		
+	}
+	
+
+
+
 }
 inf_int operator*(const inf_int& a, const inf_int& b)
 {
+	inf_int result;
 
+
+
+
+	return result;
 }
 // friend inf_int operator/(const inf_int& , const inf_int&); // not required
 
 ostream& operator<<(ostream& o, const inf_int& a)
 {
-
+	if (!a.thesign)
+		o << '-';
+	for (int i = a.length-1; i >= 0; i--){
+		o << a.digits[i];
+	}
+	return o;
 }
 // friend istream& operator>>(istream& , inf_int&);    // not required
--- prob1/prob1/main.cpp
+++ prob1/prob1/main.cpp
@@ -9,21 +9,21 @@ using namespace std;
 int main()
 {
 	inf_int a, e;
-	inf_int b(100);
+	inf_int b(-100);
 	inf_int c("321111111111122222222222233333333333444444444445555555555");
 	inf_int d("123451987651234572749499923455022211");
-	inf_int f = d;
+	inf_int f(150);
 	inf_int g(f);
 
 	// cin >> g ;   // not required
-
-	a = b*c;
+	
+	a = b+c;
 	// e=g/f;       // not required
+	b = b-f;
+	
 
-	b = c - d;
 
-
-	if (f == d) {
+	
 		cout << "a : " << a << endl;
 		cout << "b : " << b << endl;
 		cout << "c : " << c << endl;
@@ -31,7 +31,7 @@ int main()
 		// cout << "e : " << e << endl;
 		cout << "f : " << f << endl;
 		// cout << "g : " << g << endl;
-	}
+	
 
 	return 0;
 }
